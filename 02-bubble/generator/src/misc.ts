c5e709f2c604c471b7052109dc6f951db0947380
Jean-Louis GUENEGO <jlguenego@gmail.com>
2022-11-24 11:05:08 +0100

    dev: generator random with decimal
    added color random

--- 02-bubble/generator/src/misc.ts
+++ 02-bubble/generator/src/misc.ts
@@ -2,8 +2,11 @@ export const sleep = async (delay: number) => {
   return new Promise((resolve) => setTimeout(resolve, delay));
 };
 
-export const random = (min: number, max: number) => {
-  return Math.round(Math.random() * (max - min));
+export const random = (min: number, max: number, decimal = 0) => {
+  return (
+    Math.round((Math.random() * (max - min) + min) * 10 ** decimal) /
+    10 ** decimal
+  );
 };
 
 export const generateId = () => {
--- 02-bubble/generator/src/redis.ts
+++ 02-bubble/generator/src/redis.ts
@@ -3,12 +3,11 @@ import { RedisClientType } from "./interfaces/redis-type";
 import { generateId, random } from "./misc";
 
 export const insertForm = async (client: RedisClientType, form: Form) => {
-  await client.setEx(
-    "bubble:form:" + generateId(),
-    random(10, 30),
-    JSON.stringify(form)
-  );
-  const value = await client.get("toto");
+  const key = "bubble:form:" + generateId();
+  const ttl = random(10, 30);
+  console.log("ttl: ", ttl);
+  await client.setEx(key, ttl, JSON.stringify(form));
+  const value = await client.get(key);
   console.log("value: ", value);
 };
 
@@ -19,7 +18,7 @@ export const insertRandomForm = async (client: RedisClientType) => {
       cx: random(0, 100),
       cy: random(0, 100),
       r: random(0, 100),
-      color: "#eee",
+      color: `hsla(0, 100%, 50%, ${random(0, 1, 2)})`,
     },
   };
   console.log("form: ", form);
