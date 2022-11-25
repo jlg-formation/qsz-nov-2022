6f139071278c19b534cc6c5a4827734de1db1c2b
Jean-Louis GUENEGO <jlguenego@gmail.com>
2022-11-25 10:22:17 +0100

    dev: added try catch

--- 02-bubble/generator/src/redis.ts
+++ 02-bubble/generator/src/redis.ts
@@ -2,12 +2,16 @@ import type { Form, RedisClientType } from "@bubble/common";
 import { generateId, random } from "./misc";
 
 export const insertForm = async (client: RedisClientType, form: Form) => {
-  const key = "bubble:form:" + generateId();
-  const ttl = random(10, 30);
-  console.log("ttl: ", ttl);
-  await client.setEx(key, ttl, JSON.stringify(form));
-  // const value = await client.get(key);
-  // console.log("value: ", value);
+  try {
+    const key = "bubble:form:" + generateId();
+    const ttl = random(10, 30);
+    console.log("ttl: ", ttl);
+    await client.setEx(key, ttl, JSON.stringify(form));
+    // const value = await client.get(key);
+    // console.log("value: ", value);
+  } catch (err) {
+    console.log("err: ", err);
+  }
 };
 
 export const insertRandomForm = async (client: RedisClientType) => {
